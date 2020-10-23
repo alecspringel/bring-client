70349a20b403662872c4d67a4a9fad970c3b8e18
Sarah Kitten <sarahnkitten@gmail.com>
2020-10-22 23:32:01 -0700

    added form, header, submit button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Demo from "./components/demo/Demo";
+import Form from "./components/form/Form";
 
 function App() {
   return (
     <div className="App">
       <Demo />
+      <Form />
     </div>
   );
 }
