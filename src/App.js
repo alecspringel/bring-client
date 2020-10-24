9fc2f90f6791ad554ff1731a0bcda615c0aed271
Alec Springel <alecspringel@gmail.com>
2020-10-23 21:19:11 -0700

    Restructured header file, and removed ids/classes from Inputs component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Demo from "./components/demo/Demo";
-import Form from "./components/form/Form"; 
+import Form from "./components/form/Form";
 
 function App() {
   return (
     <div className="App">
       <Form />
-      <Demo />
     </div>
   );
 }
--- src/components/form/Inputs.js
+++ src/components/form/Inputs.js
@@ -6,26 +6,49 @@ class Inputs extends Component {
     return (
       <div>
         <h3>Contact Information</h3>
-        <section class="first name">
-            <input type="text" name="first name" id="first name"  placeholder="First Name" required/>
+        <section>
+          <input
+            type="text"
+            name="first name"
+            placeholder="First Name"
+            required
+          />
         </section>
         <br></br>
-        <section class="last name">
-          <input type="text" name="last name" id="last name" placeholder="Last Name" required/>
+        <section>
+          <input
+            type="text"
+            name="last name"
+            placeholder="Last Name"
+            required
+          />
         </section>
         <br></br>
-        <section class="email">
-          <input type="email" name="email" id="email" placeholder="Email" required/>
+        <section>
+          <input type="email" name="email" placeholder="Email" required />
         </section>
         <br></br>
-        <section class="phone number">
-          <input type="tel" name="phone number" id="phone number" placeholder="Phone Number" required/>
+        <section>
+          <input
+            type="tel"
+            name="phone-number"
+            placeholder="Phone Number"
+            required
+          />
         </section>
         <br></br>
-        <section class="donation description">
-          <label for="donation description">Please describe the item you would like to donate:</label>
+        <section>
+          <label for="donation-description">
+            Please describe the item you would like to donate:
+          </label>
           <br></br>
-          <textarea id="donation description" name="donation description" rows="3" cols="40" placeholder="Description" required></textarea>
+          <textarea
+            name="donation-description"
+            rows="3"
+            cols="40"
+            placeholder="Description"
+            required
+          ></textarea>
         </section>
         <br></br>
       </div>
