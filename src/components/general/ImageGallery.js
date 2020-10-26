d0e992dbb44092585a4538cf845309ee0e1b5df4
Alec Springel <alecspringel@gmail.com>
2020-10-25 23:37:33 -0700

    Created enlarged image viewer within ImageGallery and opaque background

--- src/components/general/ImageGallery.js
+++ src/components/general/ImageGallery.js
@@ -14,8 +14,10 @@ class ImageGallery extends Component {
       images: props.images,
       primary,
       current: 0,
+      enlarge: false,
     };
     this.nextImage = this.nextImage.bind(this);
+    this.toggleEnlarge = this.toggleEnlarge.bind(this);
   }
 
   static getDerivedStateFromProps(props, state) {
@@ -28,7 +30,8 @@ class ImageGallery extends Component {
     return null;
   }
 
-  nextImage(increment) {
+  nextImage(e, increment) {
+    e.stopPropagation();
     if (this.state.images.length !== 0) {
       var current = this.state.current + increment;
       if (current === this.state.images.length) {
@@ -41,26 +44,63 @@ class ImageGallery extends Component {
     }
   }
 
+  toggleEnlarge() {
+    this.setState({
+      enlarge: !this.state.enlarge,
+    });
+  }
+
   render() {
     return (
       <Wrapper>
-        <Primary src={this.state.images[this.state.current]} />
-        <Overlay>
-          <div style={{ width: "100%" }}>
+        {!this.state.enlarge && (
+          <Primary
+            src={this.state.images[this.state.current]}
+            onClick={this.toggleEnlarge}
+          />
+        )}
+        <EnlargeBackground
+          src={this.state.images[this.state.current]}
+        ></EnlargeBackground>
+        <Overlay onClick={this.toggleEnlarge}>
+          <div style={{ width: "100%", zIndex: 1 }}>
             <LeftArrow
               src={LeftArrowImg}
-              onClick={() => this.nextImage(-1)}
+              onClick={(e) => this.nextImage(e, -1)}
               show={this.state.current !== 0}
             />
             <RightArrow
               src={RightArrowImg}
-              onClick={() => this.nextImage(1)}
+              onClick={(e) => this.nextImage(e, 1)}
               show={
                 this.state.current !== this.state.images.length - 1 &&
                 this.state.images.length > 1
               }
             />
           </div>
+          {this.state.enlarge && (
+            <EnlargeWrapper>
+              <EnlargeBackground
+                src={this.state.images[this.state.current]}
+              ></EnlargeBackground>
+              <LargeImg src={this.state.images[this.state.current]} />
+              <div style={{ width: "100%", position: "absolute" }}>
+                <LargeLeftArrow
+                  src={LeftArrowImg}
+                  onClick={(e) => this.nextImage(e, -1)}
+                  show={this.state.current !== 0}
+                />
+                <LargeRightArrow
+                  src={RightArrowImg}
+                  onClick={(e) => this.nextImage(e, 1)}
+                  show={
+                    this.state.current !== this.state.images.length - 1 &&
+                    this.state.images.length > 1
+                  }
+                />
+              </div>
+            </EnlargeWrapper>
+          )}
         </Overlay>
       </Wrapper>
     );
@@ -77,12 +117,14 @@ const Wrapper = styled.div`
   align-items: center;
   justify-content: center;
   background: #000;
+  overflow: hidden;
 `;
 
 const Primary = styled.img`
   object-fit: contain;
   max-height: 100%;
   max-width: 100%;
+  z-index: 1;
 `;
 
 const Overlay = styled.div`
@@ -107,5 +149,50 @@ const LeftArrow = styled(NextArrow)`
 
 const RightArrow = styled(NextArrow)`
   float: right;
-  margin-right: 20px;
+  margin-right: 40px;
+`;
+
+const LargeLeftArrow = styled(NextArrow)`
+  float: left;
+  margin-left: 40px;
+`;
+
+const LargeRightArrow = styled(NextArrow)`
+  float: right;
+  margin-right: 40px;
+`;
+
+const EnlargeWrapper = styled.div`
+  position: fixed;
+  z-index: 2;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const EnlargeBackground = styled.img`
+  z-index: 0;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: rgb(0 0 0 / 75%);
+  filter: blur(60px);
+  -webkit-filter: blur(60px);
+  transform: scale(2);
+`;
+
+const LargeImg = styled.img`
+  max-height: 70vh;
+  max-width: 70vw;
+  height: 100%;
+  width: 100%;
+  object-fit: contain;
+  filter: blur(0);
+  -webkit-filter: blur(0);
 `;
