edeb421a61874426e6105a70977a847b5cb0ca9f
Sarah Kitten <sarahnkitten@gmail.com>
2020-10-25 21:39:03 -0700

    Form checkboxes are tabbable but not functional

--- src/components/general/Checkbox.js
+++ src/components/general/Checkbox.js
@@ -1,16 +1,33 @@
 import React from "react";
 import styled, { css } from "styled-components";
 
+// Hide checkbox visually but remain accessible to screen readers.
+// Source: https://polished.js.org/docs/#hidevisually
+
+const Checkbox = ({ className, isChecked, ...props }) => (
+  <CheckboxContainer className={className}>
+    <HiddenCheckbox checked={isChecked} {...props} />
+    <StyledCheckbox isChecked={isChecked}>
+      <Icon viewBox="0 0 24 24">
+        <polyline points="20 6 9 17 4 12" />
+      </Icon>
+    </StyledCheckbox>
+  </CheckboxContainer>
+)
+
 // Takes a prop ('isChecked': bool) to determine if the box is checked or not
+/*
 const Checkbox = (props) => {
   return (
     <Box isChecked={props.isChecked} {...props}>
-      <Icon tabindex={'0'} viewBox="0 0 24 24" {...props}>
+      <Icon viewBox="0 0 24 24" {...props}>
         <polyline points="20 6 9 17 4 12" {...props}/>
       </Icon>
     </Box>
   );
 };
+*/
+
 
 export default Checkbox;
 
@@ -21,7 +38,20 @@ const Icon = styled.svg`
   height: 17px;
 `;
 
-const Box = styled.div`
+  const HiddenCheckbox = styled.input.attrs({ type: 'checkbox' })`
+  border: 0;
+  clip: rect(0 0 0 0);
+  clippath: inset(50%);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+  `;
+
+const StyledCheckbox = styled.div`
   cursor: pointer;
   display: flex;
   align-items: center;
@@ -39,4 +69,12 @@ const Box = styled.div`
   ${Icon} {
     visibility: ${(props) => (props.isChecked ? "visible" : "hidden")};
   }
-`;
+  ${HiddenCheckbox}:focus + & {
+    box-shadow: 0 0 0 3px pink;
+  }
+  `;
+
+  const CheckboxContainer = styled.div`
+  display: inline-block;
+  vertical-align: middle;
+`
