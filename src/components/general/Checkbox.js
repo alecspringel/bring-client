d47d89a6ad57de0fd3a34627a35b606cee145ae7
Alec Springel <alecspringel@gmail.com>
2020-10-25 22:23:24 -0700

    Added invisible checkbox for accessiblity and removed dependency checkboxjs

--- src/components/general/Checkbox.js
+++ src/components/general/Checkbox.js
@@ -4,30 +4,37 @@ import styled, { css } from "styled-components";
 // Hide checkbox visually but remain accessible to screen readers.
 // Source: https://polished.js.org/docs/#hidevisually
 
-const Checkbox = ({ className, isChecked, ...props }) => (
-  <CheckboxContainer className={className}>
-    <HiddenCheckbox checked={isChecked} {...props} />
-    <StyledCheckbox isChecked={isChecked}>
-      <Icon viewBox="0 0 24 24">
-        <polyline points="20 6 9 17 4 12" />
-      </Icon>
-    </StyledCheckbox>
-  </CheckboxContainer>
-)
+// const Checkbox = ({ className, isChecked, ...props }) => (
+//   <CheckboxContainer className={className}>
+//     <HiddenCheckbox checked={isChecked} {...props} />
+//     <StyledCheckbox isChecked={isChecked}>
+//       <Icon viewBox="0 0 24 24">
+//         <polyline points="20 6 9 17 4 12" />
+//       </Icon>
+//     </StyledCheckbox>
+//   </CheckboxContainer>
+// )
 
 // Takes a prop ('isChecked': bool) to determine if the box is checked or not
-/*
 const Checkbox = (props) => {
   return (
-    <Box isChecked={props.isChecked} {...props}>
-      <Icon viewBox="0 0 24 24" {...props}>
-        <polyline points="20 6 9 17 4 12" {...props}/>
+    <Box ischecked={props.isChecked} {...props}>
+      <HiddenCheckbox ischecked={props.isChecked} {...props} />
+      <Icon
+        tabindex={"0"}
+        viewBox="0 0 24 24"
+        onClick={props.onClick}
+        name={props.name}
+      >
+        <polyline
+          points="20 6 9 17 4 12"
+          onClick={props.onClick}
+          name={props.name}
+        />
       </Icon>
     </Box>
   );
 };
-*/
-
 
 export default Checkbox;
 
@@ -38,7 +45,27 @@ const Icon = styled.svg`
   height: 17px;
 `;
 
-  const HiddenCheckbox = styled.input.attrs({ type: 'checkbox' })`
+const Box = styled.div`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  border: 1px solid #c1c1c1;
+  height: 20px;
+  width: 20px;
+  border-radius: 2px;
+  transition: all 150ms;
+  ${(props) =>
+    props.ischecked &&
+    css`
+      background-color: #f9af42;
+      border: 1px solid #ab6c0e;
+    `}
+  ${Icon} {
+    visibility: ${(props) => (props.ischecked ? "visible" : "hidden")};
+  }
+`;
+
+const HiddenCheckbox = styled.input.attrs({ type: "checkbox" })`
   border: 0;
   clip: rect(0 0 0 0);
   clippath: inset(50%);
@@ -49,32 +76,4 @@ const Icon = styled.svg`
   position: absolute;
   white-space: nowrap;
   width: 1px;
-  `;
-
-const StyledCheckbox = styled.div`
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  border: 1px solid #c1c1c1;
-  height: 20px;
-  width: 20px;
-  border-radius: 2px;
-  transition: all 150ms;
-  ${(props) =>
-    props.isChecked &&
-    css`
-      background-color: #F9AF42;
-      border: 1px solid #ab6c0e;
-    `}
-  ${Icon} {
-    visibility: ${(props) => (props.isChecked ? "visible" : "hidden")};
-  }
-  ${HiddenCheckbox}:focus + & {
-    box-shadow: 0 0 0 3px pink;
-  }
-  `;
-
-  const CheckboxContainer = styled.div`
-  display: inline-block;
-  vertical-align: middle;
-`
+`;
