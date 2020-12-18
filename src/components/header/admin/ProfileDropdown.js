ee009588ac2528cf3ca9f607e0b8f7d88c579aba
Alec Springel <alecspringel@gmail.com>
2020-12-18 01:50:41 -0800

    Created profile dropdown component

--- src/components/header/admin/ProfileDropdown.js
+++ src/components/header/admin/ProfileDropdown.js
@@ -1,19 +1,67 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
+import Arrow from "../../../assets/down-arrow-black.svg";
+import {
+  UserContext,
+  UserDispatchContext,
+} from "../../../context/UserProvider";
+import Dropdown from "../../general/dropdown/Dropdown";
 
 const ProfileDropdown = () => {
+  const history = useHistory();
+  const setUser = useContext(UserDispatchContext);
+  const user = useContext(UserContext);
+  const signOut = () => {
+    localStorage.removeItem("bringToken");
+    setUser(false);
+    history.push("/login");
+  };
+  var options = [{ label: "Sign Out", onClick: signOut }];
+
+  useEffect(() => {
+    if (user && user.isAdmin) {
+      options.unshift({ label: "Manage Users", onClick: true });
+    }
+  }, [user]);
+
   return (
-    <Profile>
-      <h1></h1>
-    </Profile>
+    <Dropdown options={options}>
+      <DropdownWrapper className="flex-row align">
+        <Profile className="profile-circle flex-row align justify">
+          <h3 className="text-reg">A</h3>
+        </Profile>
+        <ArrowImg
+          className="profile-arrow"
+          src={Arrow}
+          alt="Profile dropdown arrow"
+        />
+      </DropdownWrapper>
+    </Dropdown>
   );
 };
 
 export default ProfileDropdown;
 
+const DropdownWrapper = styled.div`
+  &:hover {
+    .profile-arrow {
+      opacity: 0.5;
+    }
+    .profile-circle {
+      box-shadow: 0 0 3px #4a4a4a;
+    }
+  }
+`;
+
 const Profile = styled.div`
   border-radius: 100px;
-  height: 100%;
+  height: 38px;
   width: 38px;
-  background: #bababa;
+  background: #e0e0e0;
+`;
+
+const ArrowImg = styled.img`
+  height: 20px;
+  opacity: 0.3;
 `;
