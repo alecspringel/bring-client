e88e4d297bbed68fd4d289d36f3ebff0b43b5049
Alec Springel <alecspringel@gmail.com>
2021-02-15 15:11:43 -0800

    Added new wording provided by BRING

--- src/components/form/Description.js
+++ src/components/form/Description.js
@@ -3,17 +3,23 @@ import React, { Component } from "react";
 class Description extends Component {
   render() {
     return (
-      <div style={{ margin: 20 }}>
-        <h2>Confirming Donations</h2>
+      <div style={{ margin: "20px 0" }}>
+        <h2>Virtual Donations Manager</h2>
         <p className="margin-tb10">
-          To confirm whether we can accept your donation, you can upload up to 5
-          photos of an item you would like to donate with a brief description.
-          Our staff will reach out to you to confirm if we can take the item or
-          not. If we can’t accept your item, we can help you find the best place
-          to take it.
+          Use the form below to send photos of your prospective donation
+          straight to our Receiving Staff. You may upload up to 5 photos per
+          item with a brief description. Our staff will review your submission
+          and confirm if we can accept your items, or if you should seek a
+          different disposal solution. If we cannot accept your materials
+          donation, we will provide you with disposal recommendations.
         </p>
-        <p className="margin-tb10">
-          Please include one item or type of item for each form submission.
+        <p className="margin-tb20">
+          Please be sure to include your contact information (on the right) with
+          photos of your materials.
+        </p>
+        <p className="margin-tb20">
+          Only one submission per item/type of item. Use “+ add another item”
+          button to make multiple submissions with the same contact information.
         </p>
         <a href="https://bringrecycling.org/things-we-cannot-take/">
           <p
